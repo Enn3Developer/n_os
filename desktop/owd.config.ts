b3944f176870afbf297bb5d3b04d8c1919011374
Enn3Developer <arturpeshko39@gmail.com>
2025-06-19 17:42:22 +0200

    added n mines

--- desktop/owd.config.ts
+++ desktop/owd.config.ts
@@ -1,7 +1,7 @@
 import { defineDesktopConfig } from '@owdproject/core/runtime/utils/utilDesktop'
 
 export default defineDesktopConfig({
-    apps: ['@owdproject/app-about', '@owdproject/app-terminal'],
+    apps: ['@owdproject/app-about', '@owdproject/app-terminal', '@enn3devplayer/owd-mines', '@owdproject/app-debug'],
     modules: [],
     theme: '@owdproject/theme-win95',
 })
