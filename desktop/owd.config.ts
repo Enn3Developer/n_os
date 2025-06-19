9038b4f161d6add5cde630af84d5bb5a38eada24
Enn3Developer <arturpeshko39@gmail.com>
2025-06-19 16:24:08 +0200

    removed todo app and added terminal

--- desktop/owd.config.ts
+++ desktop/owd.config.ts
@@ -1,7 +1,7 @@
 import { defineDesktopConfig } from '@owdproject/core/runtime/utils/utilDesktop'
 
 export default defineDesktopConfig({
-    apps: ['@owdproject/app-about', "@owdproject/app-todo"],
+    apps: ['@owdproject/app-about', '@owdproject/app-terminal'],
     modules: [],
     theme: '@owdproject/theme-win95',
 })
