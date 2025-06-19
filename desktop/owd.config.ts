7a041ac8e307d624f0a22bedf5771d0dcc70a94a
Enn3Developer <arturpeshko39@gmail.com>
2025-06-19 15:50:10 +0200

    trying to add apps

--- desktop/owd.config.ts
+++ desktop/owd.config.ts
@@ -1,7 +1,7 @@
 import { defineDesktopConfig } from '@owdproject/core/runtime/utils/utilDesktop'
 
 export default defineDesktopConfig({
-    apps: ['@owdproject/app-about'],
+    apps: ['@owdproject/app-about', "@owdproject/app-todo"],
     modules: [],
     theme: '@owdproject/theme-win95',
 })
